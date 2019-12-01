b884e2beb2eac2a9a1363523322a9a3c706f78d3
prodes13 <ciobotaru.florin@gmail.com>
2019-12-01 14:53:56 +0200

    firebase included

--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
@@ -15,13 +24,22 @@ import { requestQuestions, returnQuestion, returnGlobalIndex, saveQuestions} fro
 
 
 import * as serviceWorker from './serviceWorker';
-
+import { firebaseConfig } from './config/fbConfig';
 
 // const logger = createLogger()
 
 const rootReducers = combineReducers({requestQuestions, returnQuestion, returnGlobalIndex, saveQuestions})
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+firebase.initializeApp(firebaseConfig);
+
+console.log(firebase.database().ref().child('questions'));
+
+
+
+
 
 // const store = createStore(rootReducers, composeEnhancer(applyMiddleware(thunkMiddleware, logger)))
 const store = createStore(rootReducers, composeEnhancer(applyMiddleware(thunkMiddleware)));
@@ -31,3 +49,8 @@ ReactDOM.render(
         <App />
     </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
+
+
+// for database firebase
+
+// https://firebase.google.com/docs/database/web/structure-data
