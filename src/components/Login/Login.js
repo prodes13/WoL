9e18a8133216e3c58d6883f3702ea75c93d5df19
dumitruciolan <dumitru.ciolan@outlook.com>
2019-12-02 20:44:52 +0200

    created login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Container, Jumbotron } from 'react-bootstrap'
+import { Container, Jumbotron, Form, Button } from 'react-bootstrap'
 
 const Login = () => (
-    <Container>
-        <Jumbotron className="bg-light">
-            
-        </Jumbotron>
-    </Container>
+  <Container>
+    <Jumbotron className="bg-light">
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Adresa de email</Form.Label>
+          <Form.Control type="email" placeholder="Introdu adresa de email" />
+          <Form.Text className="text-muted">
+      </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Parola</Form.Label>
+          <Form.Control type="password" placeholder="Introdu parola" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Retine datele de logare" />
+        </Form.Group>
+        <Button variant="primary"
+                type="submit"
+                size="lg"
+                block>
+          Submit
+    </Button>
+      </Form>
+    </Jumbotron>
+  </Container>
 )
 
 export default Login;
+
