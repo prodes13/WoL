7d0018b17eafd14fc930f344d8dc448e724944cb
dumitruciolan <dumitru.ciolan@outlook.com>
2019-12-02 18:46:14 +0200

    added mockup pages for register, login and profile

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import AppNavbar from './components/Layout/AppNavbar';
 import Home from './components/Home/Home';
 import Quiz from './Quiz';
 import ShowResults from "./components/Results/ShowResults";
+import Login from "./components/Login/Login";
+import Register from './components/Register/Register';
+import Profile from './components/Profile/Profile';
 export default function App() {
   return (
     <Router>
@@ -22,6 +25,15 @@ export default function App() {
           </Route>
           <Route path="/results">
             <ShowResults />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/profile">
+            <Profile />
           </Route>
         </Switch>
     </Router>
---
+++ src/components/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container, Jumbotron } from 'react-bootstrap'
+
+const Register = () => (
+    <Container>
+        <Jumbotron className="bg-light">
+            
+        </Jumbotron>
+    </Container>
+)
+
+export default Register;
