d4666e1307b201625c1fc8570fc659508d78be3d
dumitruciolan <dumitru.ciolan@outlook.com>
2019-12-03 11:07:37 +0200

    fixed login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,27 +7,27 @@ const Login = () => (
       <Form>
         <Form.Group controlId="formBasicEmail">
           <Form.Label><b>Adresa de email*</b></Form.Label>
-          <Form.Control type="email" placeholder="Introdu adresa de email" required/>
+          <Form.Control type="email" placeholder="Introdu adresa de email" required />
           <Form.Text className="text-muted">
-      </Form.Text>
+          </Form.Text>
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
           <Form.Label><b>Parola*</b></Form.Label>
-          <Form.Control type="password" placeholder="Introdu parola" required/>
+          <Form.Control type="password" placeholder="Introdu parola" required />
         </Form.Group>
-        
+
         <br />
 
         <Button variant="primary"
-                type="submit"
-                size="lg"
-                block>
-          Autentificare 
-    </Button>
+          type="submit"
+          size="lg"
+          block>
+          Autentificare
+        </Button>
       </Form>
     </Jumbotron>
-    </Container>
+  </Container>
 )
 
 export default Login;
