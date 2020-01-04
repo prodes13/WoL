3e9d517d99bbb6fa5f9f433dfdfd3ff67c0d9349
dumitruciolan <50290361+dumitruciolan@users.noreply.github.com>
2020-01-04 11:21:58 +0200

    rebuilt questions pagination nav

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,33 +9,34 @@ class ApiCall extends React.Component {
     questions: [],
     currentQuestion: 0,
   };
-  
+
   constructor(props) {
     super(props);
 
     this.goToNextQuestion = this.goToNextQuestion.bind(this)
+    this.goToPrevQuestion = this.goToPrevQuestion.bind(this)
   }
-  
+
   componentDidMount() {
     // questions are fetched from the server
     call()
-    // porting questions into state
-    .then(data => {
-      this.setState({
-        isLoaded: true,
-        questions: data
+      // porting questions into state
+      .then(data => {
+        this.setState({
+          isLoaded: true,
+          questions: data
+        },
+        );
       },
-      );
-    },
-    // it's important to handle errors here instead of a catch() block
-    // so that we don't swallow exceptions from actual bugs in components
-    (error) => {
-      this.setState({
-        isLoaded: true,
-        error
-      });
-    }
-    )
+        // it's important to handle errors here instead of a catch() block
+        // so that we don't swallow exceptions from actual bugs in components
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   goToNextQuestion() {
@@ -43,23 +44,45 @@ class ApiCall extends React.Component {
       currentQuestion: this.state.currentQuestion + 1
     })
   }
-  
+
+  goToPrevQuestion() {
+    this.setState({
+      currentQuestion: this.state.currentQuestion - 1 
+    })
+  }
+
   render() {
     const { questions, isLoaded } = this.state;
 
-    if(!isLoaded)
+    if (!isLoaded)
       return (
         <div>loading</div>
       )
 
     const currentQuestion = questions[this.state.currentQuestion]
-    
+
     return (
       <div>
-          <p key={currentQuestion.id}>
-            {currentQuestion.name}
-          </p>
-          <button onClick={this.goToNextQuestion}>next</button>
+        <p key={currentQuestion.id}>
+          {currentQuestion.name}
+        </p>
+
+        <nav>
+          <ul className="pagination">
+            <li className="page-item"><a className="page-link" href="#" onClick={this.goToPrevQuestion}>Previous</a></li>
+            <li className="page-item"><a className="page-link" href="#">1</a></li>
+            <li className="page-item"><a className="page-link" href="#">2</a></li>
+            <li className="page-item"><a className="page-link" href="#">3</a></li>
+            <li className="page-item"><a className="page-link" href="#">4</a></li>
+            <li className="page-item"><a className="page-link" href="#">5</a></li>
+            <li className="page-item"><a className="page-link" href="#">6</a></li>
+            <li className="page-item"><a className="page-link" href="#">7</a></li>
+            <li className="page-item"><a className="page-link" href="#">8</a></li>
+            <li className="page-item"><a className="page-link" href="#">9</a></li>
+            <li className="page-item"><a className="page-link" href="#">10</a></li>
+            <li className="page-item"><a className="page-link" href="#" onClick={this.goToNextQuestion}>Next</a></li>
+          </ul>
+        </nav>
       </div>
     );
   }
