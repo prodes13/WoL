68a431b532e48396916f276f5c80c59ddb909fbe
dumitruciolan <dumitru.ciolan@outlook.com>
2020-01-11 10:19:22 +0200

    getting token and user answers

--- src/SubmitQuiz.js
+++ src/SubmitQuiz.js
@@ -1,13 +1,26 @@
 import React from 'react'
 
 class SubmitQuiz extends React.Component {
+    state = {
+        token: '',
+        results: [],
+    }
+
     constructor(props) {
         super(props)
+        this.sendAnswers = this.sendAnswers.bind(this)
+    }
+
+    componentDidMount() {
+        let results = this.props.questions
+        this.setState({
+            results: results
+        })
     }
 
     sendAnswers() {
         //insert here POST request
-        alert("sending")
+        console.log(this.state)
     }
 
     render() {
@@ -16,10 +29,15 @@ class SubmitQuiz extends React.Component {
                 <div className="alert alert-success">
                     Multumim pentru timpul acordat
                     </div>
+                <div>Insert token here:</div>
+                <input value={this.state.token} onChange={(event) => this.setState({
+                    token: event.target.value
+                })} />
+                <br />
                 <button className="btn btn-success" onClick={this.sendAnswers}>Trimite raspunsurile</button>
             </div>
 
-            
+
         )
     }
 }
