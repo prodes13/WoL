2b5e8e698c1a3f08cd9bfd5c5adea4ff1d798802
dumitruciolan <dumitru.ciolan@outlook.com>
2020-02-01 10:58:14 +0200

    removed Redux constants from the project

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,23 +1,6 @@
 import { call } from '../api'
-import {
-  REQUEST_QUESTIONS_PENDING,
-  REQUEST_QUESTIONS_SUCCESS,
-  REQUEST_QUESTIONS_FAILED,
-  RETURN_QUESTION,
-  QUESTION_INDEX_ADD,
-  QUESTION_INDEX_MIN,
-  SAVE_QUESTIONS_ANSWERS
- } from './constants'
 
 export const requestQuestions = () => (dispatch) => {
-  dispatch({ type: REQUEST_QUESTIONS_PENDING })
+  dispatch()
   call('http://localhost:3300/query')
-    .then(data => dispatch({ type: REQUEST_QUESTIONS_SUCCESS, payload: data[0].questions }))
-    .catch(error => dispatch({ type: REQUEST_QUESTIONS_FAILED, payload: error }))
 }
-
-export const saveQuestions = (answeredQuestion) => ({ type: SAVE_QUESTIONS_ANSWERS, payload: answeredQuestion});
-
-export const returnQuestion = (question) => ({type: RETURN_QUESTION, payload: question})
-export const addGlobalIndex = (globalIndex) => ({type: QUESTION_INDEX_ADD, payload: globalIndex + 1});
-export const minGlobalIndex = (globalIndex) => ({type: QUESTION_INDEX_MIN, payload: globalIndex - 1});
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,67 +1,43 @@
-import {
-    REQUEST_QUESTIONS_PENDING,
-    REQUEST_QUESTIONS_SUCCESS,
-    REQUEST_QUESTIONS_FAILED,
-    RETURN_QUESTION,
-    QUESTION_INDEX_ADD,
-    QUESTION_INDEX_MIN,
-    SAVE_QUESTIONS_ANSWERS
-   } from './constants';
-  
-  const initialIndex = {
-    globalIndex: 0
-  }
-  const initialStateQuestions = {
-    questions: [],
-    isPending: true
-  }
-  
-  export const requestQuestions = (state=initialStateQuestions, action={}) => {
-    switch (action.type) {
-      case REQUEST_QUESTIONS_PENDING:
-        return Object.assign({}, state, {isPending: true})
-      case REQUEST_QUESTIONS_SUCCESS:
-        return Object.assign({}, state, {questions: action.payload, isPending: false})
-      case REQUEST_QUESTIONS_FAILED:
-        return Object.assign({}, state, {error: action.payload})
-      default:
-        return state
-    }
-  }
+const initialIndex = {
+  globalIndex: 0
+}
+const initialStateQuestions = {
+  questions: [],
+  isPending: true
+}
 
-  const initialStateQNum = {
-    question: null
+export const requestQuestions = (state = initialStateQuestions, action = {}) => {
+  switch (action.type) {
+    default:
+      return state
   }
+}
 
-  export const returnQuestion = (state = initialStateQNum, action = {}) => {
-    switch (action.type) {
-      case RETURN_QUESTION:
-        return Object.assign({}, state, {question: action.payload})
-      default:
-        return state
-    }
-  }
-  
-  export const returnGlobalIndex = (state = initialIndex, action = {}) => {
-    switch (action.type) {
-      case QUESTION_INDEX_ADD:
-        return Object.assign({}, state, { globalIndex: action.payload })
-      case QUESTION_INDEX_MIN:
-        return Object.assign({}, state, { globalIndex: action.payload })
-      default: 
-        return state
-    }
-  }
+const initialStateQNum = {
+  question: null
+}
 
-  const initialStateAnswered = {
-    questionsAnswered: null
+export const returnQuestion = (state = initialStateQNum, action = {}) => {
+  switch (action.type) {
+    default:
+      return state
   }
+}
 
-  export const saveQuestions = (state = initialStateAnswered, action = {}) => {
-    switch (action.type) {
-      case SAVE_QUESTIONS_ANSWERS:
-        return Object.assign({}, state, {questionsAnswered: action.payload});
-      default:
-        return state;
-    }
+export const returnGlobalIndex = (state = initialIndex, action = {}) => {
+  switch (action.type) {
+    default:
+      return state
   }
+}
+
+const initialStateAnswered = {
+  questionsAnswered: null
+}
+
+export const saveQuestions = (state = initialStateAnswered, action = {}) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
