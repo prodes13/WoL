23f17ffd6f482848dfd5f55328fe5bcb8ec0f182
dumitruciolan <dumitru.ciolan@outlook.com>
2020-02-27 22:33:03 +0200

    removed mockup apiCall from home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Container, Jumbotron } from 'react-bootstrap'
-import ApiCall from '../ApiCall/ApiCall'
 
 const Home = () => (
 
     <Container>
         <Jumbotron className="bg-light">
             <img src='/logo512.png' alt='Logo' className="img-thumbnail" />
-            <ApiCall />
         </Jumbotron>
     </Container>
 )
